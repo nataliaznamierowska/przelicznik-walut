e2a7136a11202f5eebe59f62875c705623a0e748
natalia_znamierowska <nat.znamierowska@gmail..com>
2023-07-14 21:44:13 +0200

    przelicznik walut

--- script.js
+++ script.js
@@ -1,54 +1,20 @@
-// Pobieranie danych z NBP API
-function fetchData(url) {
-    return fetch(url).then(response => response.json());
-  }
-  
-  // Pobieranie aktualnych kursów walut
-  function fetchCurrencyRates() {
-    const url = 'https://api.nbp.pl/api/exchangerates/tables/A/?format=json';
-  
-    return fetchData(url);
-  }
-  
-  // Przeliczanie waluty na złotówki
-  function convertCurrency(amount, rate) {
-    return amount * rate;
-  }
-  
-  // Aktualizowanie wyników na stronie
-  function updateResults(result) {
-    const resultElement = document.getElementById('result');
-    resultElement.textContent = result.toFixed(2);
-  }
-  
-  // Obsługa przeliczania walut
-  function handleConversion() {
-    const amount = parseFloat(document.getElementById('amount').value);
-    const currency = document.getElementById('currency').value;
-  
-    fetchCurrencyRates()
-      .then(data => {
-        let rate;
-        switch (currency) {
-          case 'EUR':
-            rate = data[0].rates.find(rate => rate.code === 'EUR').mid;
-            break;
-          case 'USD':
-            rate = data[0].rates.find(rate => rate.code === 'USD').mid;
-            break;
-          case 'CHF':
-            rate = data[0].rates.find(rate => rate.code === 'CHF').mid;
-            break;
-        }
-  
-        const result = convertCurrency(amount, rate);
-        updateResults(result);
-      })
-      .catch(error => {
-        console.error('Wystąpił błąd:', error);
-      });
-  }
-  
-  // Dodawanie nasłuchiwania na przycisk
-  document.getElementById('convert').addEventListener('click', handleConversion);
-  
+document.getElementById("convertBtn").addEventListener("click", convertCurrency);
+
+function convertCurrency() {
+  var amount = document.getElementById("amount").value;
+  var currency = document.getElementById("currency").value;
+
+  var url = "https://api.nbp.pl/api/exchangerates/rates/A/" + currency + "/?format=json";
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      var exchangeRate = data.rates[0].mid;
+      var result = amount * exchangeRate;
+      document.getElementById("result").textContent = result.toFixed(2) + " PLN";
+    })
+    .catch(error => {
+      console.log(error);
+      document.getElementById("result").textContent = "Error fetching exchange rate.";
+    });
+}
