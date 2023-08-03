379b1f52bfe736b1e6334d907b26d15f67a0d263
natalia_znamierowska <nat.znamierowska@gmail..com>
2023-08-04 01:03:53 +0200

    master

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-document.getElementById("convertBtn").addEventListener("click", convertCurrency);
+const convertBtn = document.getElementById('convertBtn')
+const result = document.getElementById('result')
 
-function convertCurrency() {
-  var amount = document.getElementById("amount").value;
-  var currency = document.getElementById("currency").value;
+convertBtn.addEventListener('click', convertCurrency)
 
-  var url = "https://api.nbp.pl/api/exchangerates/rates/A/" + currency + "/?format=json";
+function convertCurrency(event) {
+	event.preventDefault()
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      var exchangeRate = data.rates[0].mid;
-      var result = amount * exchangeRate;
-      document.getElementById("result").textContent = result.toFixed(2) + " PLN";
-    })
-    .catch(error => {
-      console.log(error);
-      document.getElementById("result").textContent = "Error fetching exchange rate.";
-    });
+	const amount = parseFloat(document.getElementById('amount').value)
+	const currency = document.getElementById('currency').value
+
+	if (isNaN(amount) || amount < 0.01) {
+		result.textContent = 'Wprowadź poprawną kwotę (min. 0.01).'
+		return
+	}
+
+	const url = 'https://api.nbp.pl/api/exchangerates/rates/A/' + currency + '/?format=json'
+
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			const exchangeRate = data?.rates?.[0]?.mid
+
+			if (exchangeRate) {
+				const resultValue = (amount * exchangeRate).toFixed(2)
+				result.textContent = resultValue + ' PLN'
+			} else {
+				result.textContent = 'Nie udało się pobrać kursu waluty.'
+			}
+		})
+		.catch(error => {
+			console.log(error)
+			result.textContent = 'Wystąpił błąd przy pobieraniu kursu waluty.'
+		})
 }
